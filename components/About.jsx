77bcb3d498b23ca51d39da03ac7edbc27fb93aa0
blur555555 <42386276+blur555555@users.noreply.github.com>
2022-03-19 01:36:58 +0300

    dc
    dc

--- components/About.jsx
+++
@@ -1,9 +0,0 @@
-const About = () => {
-	return (
-		<div className="word">
-			<h1>About</h1>
-		</div>
-	);
-};
-
-export { About };
--- components/Users.jsx
+++
@@ -1,9 +0,0 @@
-const Users = () => {
-	return (
-		<div className="word">
-			<h1>Users</h1>
-		</div>
-	);
-};
-
-export { Users };
